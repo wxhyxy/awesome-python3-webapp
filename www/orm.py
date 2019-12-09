89c1b4b3fb009a7e111a121e0a3f96c0a8243f04
web <w.3097652.rt@163.com>
2019-12-09 16:19:43 +0800

    添加orm字段

--- www/orm.py
+++ www/orm.py
@@ -36,12 +36,75 @@ async def select(sql, args, size=None):
 			return rs
 
 # 要执行INSERT、UPDATE、DELETE语句，可以定义一个通用的execute()函数，因为这3种SQL的执行都需要相同的参数，以及返回一个整数表示影响的行数：
-async def execute(sql, args):
+async def execute(sql, args, autocommit=True):
 	log(sql)
 	async with __pool.get as conn:
+		if not autocommit:
+			await conn.begin()
 		try:
 			async with conn.coursor() as cur:
-				affected = cur.rowcount()
+				await cur.execute(sql.execute(sql.replace('?', '%s'), args))
+				affected = cur.rowcount
+			if not autocommit:
+				await conn.begin()
 		except BaseException as e:
+			if not autocommit:
+				await conn.rollback()
 			raise 
 		return affected
+
+
+def craete_args_string(num):
+	l = []
+	for n in range(num):
+		l.append('?')
+	return ','.join(l)
+
+
+class Field(object):
+
+	def __init__(self, name, column_type, primary_key, default):
+		self.name = name
+		self.column_type = column_type
+		self.primary_key = primary_key
+		self.default = default
+
+
+	def __str__(self):
+		return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+
+	def __init__(self, name = None, primary_key=False, default=None, ddl='varchar(100)'):
+		super().__init__(name, ddl, primary_key, default)
+
+class BooleanField(Field):
+
+	def __init__(self, name = None, default=False):
+		super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+	def __init__(self, name = None, primary_key=False, default=0):
+		super.__init__(self, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+	def __init__(self, name=None, primary_key=False, default=0.0):
+		super.__init__(self, 'real',primary_key, default)
+
+class TextField(Field):
+
+	def __init__(self, name = None, default=None):
+		
+
+
+
+
+
+
+
+
+
+
